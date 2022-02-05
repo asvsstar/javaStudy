79f41fd96f69d295bf887a1d542718cfa34d16c1
asvsstar <akrwosla@naver.com>
2022-02-05 16:10:50 +0900

    이항연산자

---
+++ 2022-02-05-연산자/src/com/it/main/MainClass3.java
@@ -0,0 +1,13 @@
+package com.it.main;
+//이항연산자
+public class MainClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//국어,영어,수학 => 평균, 총점
+		int kor=90,eng=87,math=78;
+		System.out.println(kor+eng+math);
+		System.out.println((kor+eng+math)/3);
+	}
+
+}
