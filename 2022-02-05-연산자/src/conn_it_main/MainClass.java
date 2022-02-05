ff802bee274133a570b866e5432b9e84d07ce8c1
asvsstar <akrwosla@naver.com>
2022-02-05 14:55:30 +0900

    check

--- 2022-02-05-연산자/src/conn_it_main/MainClass.java
+++ 2022-02-05-연산자/src/conn_it_main/MainClass.java
@@ -5,10 +5,17 @@ public class MainClass {
 	/*
 	 * 멤버변수 : 객체마다 따라 사용하는 변수
 	 * 			한개를 가지고 공유하는 변수
+	 * 		=> 자동 초기화 =
+	 * 			int => 0
+	 * 			dobule => 0.0
+	 * 			boolean => false
+	 * 			클래스 => null
+	 * 
 	 * 지역변수 : 메소드안에서만 사용
 	 * 			지역변수 /매개변수
+	 * 			=>사용시에는 반드시 초기화후 사용
 	 */
-	s
+	
 	
 	public static void main(String[] args) {
 		
