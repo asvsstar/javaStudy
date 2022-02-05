6aed11849f2557af1fcbb47cc0fae344cbb8e23f
asvsstar <akrwosla@naver.com>
2022-02-05 14:44:45 +0900

    ㅇㅇㅇㅇ

--- 2022-02-05-연산자/src/conn_it_main/MainClass.java
+++ 2022-02-05-연산자/src/conn_it_main/MainClass.java
@@ -1,7 +1,12 @@
 package conn_it_main;
 
 public class MainClass {
-	//프로그램에 필요한 변수 설정 위치
+	//프로그램에 필요한 변수 설정 위치 (멤버변수) => 프로그램 종료시까지 메모리에 유지
+	/*
+	 * 멤버변수 : 객체마다 따라 사용하는 변수
+	 * 			한개를 가지고 공유하는 변수
+	 */
+	
 	public static void main(String[] args) {
 		
 	}
