da50dc6e7da0d26d2deadf847e285266521c052e
asvsstar <akrwosla@naver.com>
2022-02-05 16:23:11 +0900

    나머지

--- 2022-02-05-연산자/src/com/it/main/MainClass3.java
+++ 2022-02-05-연산자/src/com/it/main/MainClass3.java
@@ -5,9 +5,17 @@ public class MainClass3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//국어,영어,수학 => 평균, 총점
-		int kor=90,eng=87,math=78;
+		int kor=90,eng=87,math=77;
 		System.out.println(kor+eng+math);
 		System.out.println((kor+eng+math)/3);
+		System.out.printf("%.2f",(kor+eng+math)/3.0);//C언어
+		//1.8 => SUN, 1.9 => oracle(C언어)
+		
+//		System.out.println(10/0);
+		System.out.println(5%2); //1
+		System.out.println(-5%2);//-1
+		System.out.println(-5%-2);//-1
+		System.out.println(5%-2);//1
 	}
 
 }
