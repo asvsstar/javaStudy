5b5c2801c63ea615f414bb6efd4cb5fa34d8e27a
asvsstar <akrwosla@naver.com>
2022-02-12 16:20:38 +0900

    버블정렬 잘안됨 수업끝나고 확인해야함

--- 220212-데이터묶기(배열,클래스)/src/com/it/main/MainClass2.java
+++ 220212-데이터묶기(배열,클래스)/src/com/it/main/MainClass2.java
@@ -14,10 +14,12 @@ public class MainClass2 {
 		// TODO Auto-generated method stub
 		int[] arr=new int[5]; // arr[0]~arr[4]
 		// arr[0]~arr[4] => 임의수를 대입
+		
 		for(int i=0;i<arr.length;i++)
 		{
 			arr[i]=(int)(Math.random()*100)+1;
 		}
+		/*
 		//저장된 값 출력
 		System.out.println(Arrays.toString(arr));
 		Arrays.sort(arr);
@@ -25,11 +27,51 @@ public class MainClass2 {
 		{
 			System.out.print(i+" ");}
 		System.out.println();
-			for(int i=arr.length-1;i>=0; i--)
-			{
-				System.out.print(arr[i]+" ");
-			}
+		for(int i=arr.length-1;i>=0; i--)
+		{
+			System.out.print(arr[i]+" ");
+		}*/
+		System.out.print("정렬 전 : ");
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+			
+		}
+		System.out.println();
+		System.out.print("정렬 후(ASC:선택정렬):");
 		
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i]>arr[j])
+				{
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		for(int i:arr) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		System.out.print("정렬 후(DESC:버블정렬):");
+		for(int i=0;i<arr.length-1-i;i++)
+		{
+			for(int j=i;j<arr.length-1;j++)
+			{
+				if(arr[j]<arr[j+1])
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+	
+	for(int i:arr) {
+		System.out.print(i+" ");
+	}
 	}
 
 }
