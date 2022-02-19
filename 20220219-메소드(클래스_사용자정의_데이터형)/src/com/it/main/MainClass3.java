201ece60d07d421ddfe2c5f3fcf2b3c3b93a8844
asvsstar <akrwosla@naver.com>
2022-02-19 12:35:36 +0900

    SQL 연결작업 java/db connetion

---
+++ 20220219-메소드(클래스_사용자정의_데이터형)/src/com/it/main/MainClass3.java
@@ -0,0 +1,23 @@
+package com.it.main;
+import java.sql.*;
+/*
+ *  1. 드라이버 등록 => Class.forName("com.mysql.cj.jadbc.Driver")
+ *	2. SQL문장 제작 => SELECT/INSERT/UPDATE/DELETE
+ *	2-2. MySQL연결=> Connetion
+ *	3. MySQL로 전송=> PrepareStatment
+ *	4. 실행을 요청=> ResultSet
+ *	5. 실행된 데이터 읽기
+ */
+public class MainClass3 {
+	//MYSQL연결 => 메소드
+	//결과값이 없는 경우( 메소드안에서 자체처리) => void
+	public static void mysqlConnet()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
